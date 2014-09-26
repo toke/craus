2a73e6e99ec2204de66ccb1e48923dea112827fd
Thomas Kerpe <thomas.kerpe@1und1.de>
2014-09-26 13:28:59 +0200

    fixed indentation (broken by c\&p)

--- kraus.c
+++ kraus.c
@@ -11,32 +11,32 @@
  
 int main(int argc, char *argv[])
 {
-time_t current_time;
-struct tm *kd;
-int deltad = 0; // delta in days from current day
-unsigned int count = 1; // number of results
-int i;
-kraus_t floor;
- 
-// Optional handling of arguments (delta days and count)
-if (argc > 1) {
-deltad = atoi(argv[1]);
-if (argc > 2) {
-count = atoi(argv[2]);
-}
-}
-time(&current_time);
-current_time += deltad;
- 
-for (i=0; i < count; i++) {
-kd = localtime(&current_time);
-kd->tm_mday += deltad + i;
-mktime(kd);
-floor = kraus_floor(kd);
-(void) printf("Am %02i.%02i treffen wir uns in Stockwerk %i\n",
-kd->tm_mday, kd->tm_mon + 1, floor);
-}
-return 0;
+    time_t current_time;
+    struct tm *kd;
+    int deltad = 0; // delta in days from current day
+    unsigned int count = 1; // number of results
+    int i;
+    kraus_t floor;
+     
+    // Optional handling of arguments (delta days and count)
+    if (argc > 1) {
+        deltad = atoi(argv[1]);
+        if (argc > 2) {
+            count = atoi(argv[2]);
+        }
+    }
+    time(&current_time);
+    current_time += deltad;
+     
+    for (i=0; i < count; i++) {
+        kd = localtime(&current_time);
+        kd->tm_mday += deltad + i;
+        mktime(kd);
+        floor = kraus_floor(kd);
+        (void) printf("Am %02i.%02i treffen wir uns in Stockwerk %i\n",
+        kd->tm_mday, kd->tm_mon + 1, floor);
+    }
+    return 0;
 }
  
  
@@ -44,20 +44,20 @@ return 0;
  
 kraus_t kraus_floor(struct tm *date)
 {
-unsigned short perl_seed[2] = {0};
-double rand_number = 0;
-kraus_t floor;
- 
-// The S.U.P.E.R. secret Algorithm (Part 1)
-perl_seed[0] = (date->tm_mday * date->tm_mday + date->tm_mon + 1);
- 
-// Seed like perl 5.6 does
-unsigned short seed[3] = {SEEDMAGIC, perl_seed[0], perl_seed[1]};
-seed48(&seed[0]);
-rand_number = drand48();
- 
-// The S.U.P.E.R. secret Algorithm (Part 2)
-floor = (rand_number * MAX_FLOOR) + MIN_FLOOR;
- 
-return floor;
+    unsigned short perl_seed[2] = {0};
+    double rand_number = 0;
+    kraus_t floor;
+     
+    // The S.U.P.E.R. secret Algorithm (Part 1)
+    perl_seed[0] = (date->tm_mday * date->tm_mday + date->tm_mon + 1);
+     
+    // Seed like perl 5.6 does
+    unsigned short seed[3] = {SEEDMAGIC, perl_seed[0], perl_seed[1]};
+    seed48(&seed[0]);
+    rand_number = drand48();
+     
+    // The S.U.P.E.R. secret Algorithm (Part 2)
+    floor = (rand_number * MAX_FLOOR) + MIN_FLOOR;
+     
+    return floor;
 }
